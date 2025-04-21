06ab20da35e549cef1c0be9922d96f92318ca792
Ryan87960 <zack87960@gmail.com>
2025-04-21 14:40:07 +0800

    介面設計
    介面設計

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnopen = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnopen
+            // 
+            this.btnopen.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnopen.Location = new System.Drawing.Point(29, 23);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(116, 40);
+            this.btnopen.TabIndex = 0;
+            this.btnopen.Text = "開啟檔案";
+            this.btnopen.UseVisualStyleBackColor = true;
+            this.btnopen.Click += new System.EventHandler(this.btnopen_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnsave.Location = new System.Drawing.Point(163, 23);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(117, 40);
+            this.btnsave.TabIndex = 0;
+            this.btnsave.Text = "儲存答案";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(29, 78);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(755, 347);
+            this.rtbText.TabIndex = 1;
+            this.rtbText.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnopen);
+            this.Name = "Form1";
+            this.Text = "記事本";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnopen;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
