6600516cf9b32776f6a2dd79c7a7e9e75d9df0d6
Ryan87960 <zack87960@gmail.com>
2025-04-28 15:32:51 +0800

    目前的文字加入堆疊
    建立堆疊

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.rtbText = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listUndo = new System.Windows.Forms.ListBox();
+            this.btnUndo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnopen
@@ -61,20 +63,42 @@
             // 
             this.rtbText.Location = new System.Drawing.Point(29, 78);
             this.rtbText.Name = "rtbText";
-            this.rtbText.Size = new System.Drawing.Size(755, 347);
+            this.rtbText.Size = new System.Drawing.Size(435, 347);
             this.rtbText.TabIndex = 1;
             this.rtbText.Text = "";
+            this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listUndo
+            // 
+            this.listUndo.FormattingEnabled = true;
+            this.listUndo.ItemHeight = 15;
+            this.listUndo.Location = new System.Drawing.Point(504, 78);
+            this.listUndo.Name = "listUndo";
+            this.listUndo.Size = new System.Drawing.Size(255, 349);
+            this.listUndo.TabIndex = 2;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnUndo.Location = new System.Drawing.Point(301, 23);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(117, 40);
+            this.btnUndo.TabIndex = 0;
+            this.btnUndo.Text = "還原";
+            this.btnUndo.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listUndo);
             this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnUndo);
             this.Controls.Add(this.btnsave);
             this.Controls.Add(this.btnopen);
             this.Name = "Form1";
@@ -90,6 +114,8 @@
         private System.Windows.Forms.RichTextBox rtbText;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox listUndo;
+        private System.Windows.Forms.Button btnUndo;
     }
 }
 
